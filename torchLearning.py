a929d5288e520efb0eb5dc472be706d5f9b5d17a
feishuoren <angela335889390@gmail.com>
2022-03-20 22:55:00 +0800

    增加功能函数

--- torchLearning.py
+++ torchLearning.py
@@ -535,3 +535,19 @@ def fit_rec_sc(net,criterion,optimizer,train_data,test_data,scheduler,epochs = 3
         test_l.append(eva(test_data, net).detach())
         
     return train_l, test_l
+
+def plotsample(data):
+    """让每个数据集随机显示5张图像
+    """
+    
+    fig,axs = plt.subplots(1,5,figsize=(10,10)) # 建立子图
+    for i in range(5):
+        num = random.randint(0,len(data)-1) # 首先选取随机数，随机选取5次
+        # 抽取数据中对应的图像对象，make_grid函数可将任意格式的图像的通道数升为3，而不改变图像原始的数据
+        # 而展示图像用的imshow函数最常见的输入格式也是3通道
+        npimg = torchvision.utils.make_grid(data[num][0]).numpy()
+        nplabel = data[num][1] # 提取标签
+        # 将图像由（3，weight，height）转化为（weight,height,3），并放入imshow函数中读取
+        axs[i].imshow(np.transpose(npimg,(1,2,0)))
+        axs[i].set_title(nplabel) # 给每个子图加上标签
+        axs[i].axis("off") # 消除每个子图的坐标轴
